2d4f8368de98b25493598fa8dee019a341c3448e
Alberto Vena <kennyadsl@gmail.com>
2014-06-21 19:41:41 +0200

    refactors scopes to remove deprecations warnings

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -1,7 +1,8 @@
 if Product.table_exists?
   Product.class_eval do
     acts_as_list
-    default_scope :order => "products.position"
-    named_scope :ordered, :order => 'products.position'
+  
+    scope :ordered, :order => 'products.position'
+    default_scope ordered
   end
 end
